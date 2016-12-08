695d17fdb36e77e01021af2dd36210dc52265026
MarwaBj <MarwaBj@192.168.1.2>
2016-12-08 22:53:10 +0100

    Valider un contrat
    Signed-off-by: MarwaBj <MarwaBj@192.168.1.2>

--- AssureTout/AssureTout-web/src/main/java/tn/esprit/assuretout/managedbean/RedacteurBean.java
+++ AssureTout/AssureTout-web/src/main/java/tn/esprit/assuretout/managedbean/RedacteurBean.java
@@ -1,12 +1,74 @@
 package tn.esprit.assuretout.managedbean;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import tn.esprit.assuretout.contracts.ContratBienGarantieServices;
+import tn.esprit.assuretout.contracts.ContratServices;
+import tn.esprit.assuretout.contracts.GarantieSousGarantieNiveauServices;
+import tn.esprit.assuretout.entities.Contrat;
+import tn.esprit.assuretout.entities.ContratBienGarantie;
+import tn.esprit.assuretout.entities.GarantieSousGarantieNiveau;
+
 
 @ManagedBean
 @ViewScoped
 public class RedacteurBean {
 
+	@EJB
+	GarantieSousGarantieNiveauServices service;
+
+	@EJB
+	ContratBienGarantieServices serviceC;
+	
+	@EJB
+	ContratServices contratService;
+	
+	private int step=0;
+	
+	private List<ContratBienGarantie> list;
+	
+	private List<GarantieSousGarantieNiveau> listSG = new ArrayList<GarantieSousGarantieNiveau>();
+
+	@PostConstruct
+	public void init() {
+		list = serviceC.findAll();
+	}
+	
+	public String doValiderContrat(Contrat c){
+		c.setEtat(true);
+		contratService.update(c);
+		return "/pages/Redacteur/gestionContrat?faces-redirect=true";
+	}
+
+	public List<ContratBienGarantie> getList() {
+		return list;
+	}
+
+	public void setList(List<ContratBienGarantie> list) {
+		this.list = list;
+	}
+
+	public List<GarantieSousGarantieNiveau> getListSG() {
+		return listSG;
+	}
+
+	public void setListSG(List<GarantieSousGarantieNiveau> listSG) {
+		this.listSG = listSG;
+	}
+
+	public int getStep() {
+		return step;
+	}
+
+	public void setStep(int step) {
+		this.step = step;
+	}
+	
 	
 }
